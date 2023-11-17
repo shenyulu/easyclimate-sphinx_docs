29e4a9a75440bd1eef128bc7a4c3d80b4c75e852
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2023-11-17 18:40:25 +0800

    Delete temp file in main repository

--- clean.ps1
+++ clean.ps1
@@ -1,2 +1,8 @@
 Remove-Item -Path ".\source\auto_gallery_output\*" -Recurse
 Remove-Item -Path ".\source\api_index\generated\*"
+
+# Delete temp file in main repository
+$local_path = Get-Location
+Set-Location ..\easyclimate
+python -u .\delete_pycache.py
+Set-Location $local_path
